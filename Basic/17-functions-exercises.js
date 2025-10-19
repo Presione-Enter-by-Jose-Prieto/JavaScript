feaabca6d8b210f1de4a0a10f4937ad76b1519fd
Jose Prieto <alejandroprietosal@gmail.com>
2025-10-19 16:11:32 -0500

    Curso de JS - Ejercicios de funciones

--- Basic/17-functions-exercises.js
+++ Basic/17-functions-exercises.js
@@ -45,18 +45,114 @@ console.log(contarVocales(myString))
 
 let myArrayStrings = ['Hola Mundo', 'Hola Js', 'Hola JavaScript', 'Hola José']
 function arraytoUpperCase(myArray){
-    
+    let myArrayStringsMayus = []
+    for(let string of myArray){
+        myArrayStringsMayus.push(string.toUpperCase())
+    }
+    return myArrayStringsMayus
 }
+
 console.log(arraytoUpperCase(myArrayStrings))
 
 // 5. Crea una función que reciba un número y devuelva true si es primo, y false en caso contrario
 
+function esPrimo(num){
+    let verdadero = true
+    let cont = 0
+    for(let i = 0; i <= num; i++){
+        if(num % i == 0){
+            cont++
+        }
+    }
+    if(cont == 2){
+        return verdadero
+    }else{
+        return verdadero = false
+    }
+}
+
+console.log(esPrimo(6))
+
 // 6. Crea una función que reciba dos arrays y devuelva un nuevo array que contenga los elementos comunes entre ambos
+
+function comunElements(arrayUno, arrayDos){
+    let newArray = []
+    for(let elementoComun1 of arrayUno){
+        for(let elementoComun2 of arrayDos){
+            if(elementoComun1 == elementoComun2){
+                newArray.push(elementoComun1)
+            }
+        }
+    }
+    return newArray
+}
+
+let arrayUno = [1,3,5,7,9,10]
+let arrayDos = [2,4,6,8,10]
+
+console.log(comunElements(arrayUno, arrayDos))
 
 // 7. Crea una función que reciba un array de números y devuelva la suma de todos los números pares
 
+function sumPares(array){
+    let suma = null 
+    for (let num of array){
+        if (num % 2 == 0) suma += num
+    }
+    return suma
+}
+
+let arrayNums = [1,2,3,4,6,8]
+console.log(sumPares(arrayNums))
+
 // 8. Crea una función que reciba un array de números y devuelva un nuevo array con cada número elevado al cuadrado
+
+function elevaAlCuadrado(array){
+    let arayAlCuadrado = []
+    let cuadrado = null
+    for (let num of array){
+        cuadrado = num**2 
+        arayAlCuadrado.push(cuadrado)
+    }
+    return arayAlCuadrado
+}
+
+let arraySinElevar = [1,2,3,4,5,6]
+console.log(elevaAlCuadrado(arraySinElevar))
 
 // 9. Crea una función que reciba una cadena de texto y devuelva la misma cadena con las palabras en orden inverso
 
+function stringInverso(string){
+    let stringInverse = null
+    stringInverse = string.split(' ').reverse().join(' ')
+
+    return stringInverse
+}
+
+let string = 'Hola Mundo'
+console.log(stringInverso(string))
+
+/*
+    NOTA DEL EJERCICIO:
+    El método split() convierte una cadena (string) en un arreglo (array),
+    dividiendo el texto en cada punto donde se indique el delimitador (por ejemplo, split(' ') separa por espacios).
+
+    El método reverse() invierte el orden de los elementos dentro del arreglo.
+
+    El método join() une todos los elementos del arreglo en una sola cadena,
+    insertando entre ellos el delimitador especificado (por ejemplo, join(' ') los separa con un espacio). 
+    En conjunto, split() → reverse() → join() permiten invertir el orden de las palabras en una cadena.
+*/
+
+
 // 10. Crea una función que calcule el factorial de un número dado
+
+function factorialDeUnNumero(num){
+    let factorial = 1
+    for(let i = 1 ; i <= num; i++){
+        factorial *= i
+    }
+    return factorial
+}
+
+console.log(factorialDeUnNumero(5))
